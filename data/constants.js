c45dbc89fa82c2daf6cff8a7bfadf2c53d58380d
p-crys <56489083+p-crys@users.noreply.github.com>
2024-07-17 03:40:03 -0400

    added project files
    added project files

---
+++ data/constants.js
@@ -0,0 +1,225 @@
+export const Bio = {
+  name: "Crystal Phiri",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. I appreciate not only the complexities of computers and their functioning, but also the opportunity to engage with different industries and pursue my diverse interests.",
+  github: "https://github.com/p-crys",
+  resume: "https://docs.google.com/document/d/1H9KWQQwxADUjJNL-7sYG_B1wmjUf6o1X/edit?usp=sharing&ouid=102358609789498187175&rtpof=true&sd=true",
+  linkedin: "https://www.linkedin.com/in/cphiri3/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "D3",
+        image:
+          "https://ih1.redbubble.net/image.482086039.3183/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.jpg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Drupal",
+        image:
+          "https://w7.pngwing.com/pngs/108/364/png-transparent-drupal-hd-logo-thumbnail.png",
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "GraphQL",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      
+    ],
+  },
+
+  {
+    title: "Data Analysis",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tableau",
+        image:
+          "https://keycombiner.com/media/application-icons/tableau.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {    
+    id: 0,
+    img: "https://cdn11.bigcommerce.com/s-n50qw/images/stencil/1280x1280/products/12656/18896/GT-image-decal__05727.1680096876.jpg?c=2",
+    role: "Bachelor of Computer Science",
+    company: "Georgia Institute of Technology",
+    date: "Aug 2019 - May 2024",
+    desc: "Completes Bachelors of Computer Science with threads in People and Media. Taken courses in Data Structures, Algorithms, Object-Oriented Programming, and Data Analysis, among others. GPA: 3.34",
+    skills: [
+      "Java",
+      "Python",
+      "Web Development",
+      "Game Design",
+      "Sonification",
+      "Data Analysis",
+      "OOP"
+    ]
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4D0BAQH3NM5VM_X4aQ/company-logo_200_200/0/1630558111506?e=2147483647&v=beta&t=40xlSrq6_QOsXGIi1hesgRl-Lr8Sl-KxtwDpthNg-L4",
+    role: " Research Assistant",
+    company: "Bee Snap Machine Learning VIP Lab",
+    date: "Aug 2021 - Dec 2022",
+    desc: "Collaborates in a team to analyze bee flight data and study bee activity. Utilizes Python to preprocess data, model, and optimize neural networks. Employs NumPy and Pandas for data analysis . Won 1st Place in Systems, People, and Environment at the 6th Annual GT Innovation Competition.",
+    skills: [
+      "Python",
+      "NumPy",
+      "Panda",
+      "Juypter",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/en/9/98/Robert_C._Williams_Paper_Museum_Logo.png",
+    role: "Student Assistant",
+    company: "Robert C. Williams Museum of Papermaking",
+    date: "Aug 2020 - May 2023",
+    desc: "Provides guidance and support to participants of all ages during papermaking workshops. Creates, designs, and updates museum’s website pages. Develops, restores, and photographs over 100 artifacts for archive. Develops, restores, and photographs over 100 artifacts for archive",
+    skills: [
+      "Drupal",
+      "HTML",
+      "CSS",
+    ],
+  
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZoKutsOW0qUtxeSWB-MO0jIbnhOmK6zy8CA&s",
+    role: "Software Engineer Intern",
+    company: "Publicis Sapient",
+    date: "June 2022 - Aug 2022",
+    desc: "Improve search optimization by using SearchIQ and modifying GraphQL queries in a client project. Creates a recipe to implement SearchIQ and increases search aggregation on client’s pages. Completes Full Stack Development Bootcamp focused on focused on: DevOps, Security & Risk Management, Java & Web Development, Databases, APIs, Microservices & Testing.",
+    skills: [
+      "GraphQL",
+      "Bitbucket",
+    ],
+  },
+
+];
+
+
+export const projects = [
+  {
+    id: 0,
+    title: "Yoga Audio Aid Simulator",
+    date: "Oct 2023- Dec 2023",
+    description:
+      "Designs a system that provides real-time feedback assessing the execution of the user’s yoga pose through sonification. Utilizes Python, Processing, BEADS, and Data and Event Handling",
+    image:
+      "https://i.postimg.cc/gJYMSkJR/yoga-simulator.png",
+    tags: ["Python", "Processing", "BEADS", "Express Js", "Data and Event Handling", "Sonification" ],
+    github: "https://github.com/p-crys/yoga_simulator",
+  },
+  {
+    id: 1,
+    title: "Valoria's Quest: Princess's Rescue​",
+    date: "Jan 2024 - May 2024",
+    description:
+      "Collaborates in a team to develop a 3D world horror maze game. Designs enemies’ physics-based movement, state-machine based AI, Mecanim animations, and collectible objects. Utilizes Unity, C#, and Github for version control",
+    image:
+      "https://i.postimg.cc/tg1R1py3/Valoria.png",
+    tags: [
+      "Unity",
+      "C#",
+      "Github",
+      "Mecanim animations",
+      "AI",
+    ],
+    github: "https://github.com/poojapache/ValoriaQuestPrincessRescue",
+  },
+  {
+    id: 2,
+    title: "Aircraft Incident Scrollytelling",
+    date: "Dec 2023",
+    description:
+      "Creates an interactive visualization that details facts about 1987 aircraft incidents as the user navigates through the webpage. Utilizes HTML, CSS, Javascript, Scroller.js, and D3.js ",
+    image:
+      "https://i.postimg.cc/zX9gDtTP/scrolly-telling.png",
+    tags: ["HTML", "CSS", "Javascript", "D3.js", "Scroller.js", "Interactive Visualization"],
+    github: "https://github.com/p-crys/scrolltelling",
+  },
+  {
+    id: 3,
+    title: "Classy-Cal Protoype",
+    date: "Jan 2024 - Apr 2024",
+    description:
+      "Design a smart calendar to help college students plan out their schedule, using AI. Utilizes HTML, CSS, and Javascript",
+    image:
+      "https://i.postimg.cc/MH59Jkn4/Classy-Cal.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/p-crys/Classy-Cal",
+  },
+];
+
